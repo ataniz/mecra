5f76bb02b5a5e9630ac01065a20cc874695131b1
Ata Nizamoglu <nizamoglu@campus.tu-berlin.de>
2021-04-17 23:35:13 +0300

    login/signup

--- client/src/components/layout/Login.js
+++ client/src/components/layout/Login.js
@@ -1,7 +1,65 @@
-import React from 'react';
+import { Fragment, React, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Button, Card, Container, Row } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserAstronaut } from '@fortawesome/free-solid-svg-icons';
 
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Card className="mt-4 border-0">
+      <Container
+        className="mt-4 border-0 clearfix"
+        style={{ display: 'inline' }}
+      >
+        <Row>
+          <FontAwesomeIcon icon={faUserAstronaut} size="4x" />
+          <h1 style={{ color: 'black', marginLeft: '2rem' }}>Kaydol</h1>
+        </Row>
+      </Container>
+      <Form className="mt-4" onSubmit={onSubmit}>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="E-Posta Adresi"
+            value={email}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Şifre"
+            value={password}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Giriş Yap
+        </Button>
+        <p className="mt-2">
+          <Link to="/register">Mecra'ya Kaydol</Link>
+        </p>
+      </Form>
+    </Card>
+  );
 };
 
 export default Login;
--- client/src/components/layout/Register.js
+++ client/src/components/layout/Register.js
@@ -1,9 +1,30 @@
-import { React, Fragment } from 'react';
-import { Form, Button, Card, Container, Row, Col } from 'react-bootstrap';
+import { React, useState } from 'react';
+import { Form, Button, Card, Container, Row } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserAstronaut } from '@fortawesome/free-solid-svg-icons';
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log('Şifreler Uyumsuz!');
+    } else {
+      console.log(formData);
+    }
+  };
+
   return (
     <Card className="mt-4 border-0">
       <Container
@@ -15,23 +36,50 @@ const Register = () => {
           <h1 style={{ color: 'black', marginLeft: '2rem' }}>Kaydol</h1>
         </Row>
       </Container>
-      <Form className="mt-4">
+      <Form className="mt-4" onSubmit={onSubmit}>
         <Form.Group controlId="Name">
-          <Form.Control placeholder="İsminiz" />
+          <Form.Control
+            type="text"
+            name="name"
+            placeholder="İsim"
+            value={name}
+            onChange={(e) => onChange(e)}
+            required
+          />
           <Form.Text className="text-muted"></Form.Text>
         </Form.Group>
         <Form.Group controlId="formBasicEmail">
-          <Form.Control type="email" placeholder="E-Posta Adresiniz" />
-          <Form.Text className="text-muted"></Form.Text>
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="E-Posta Adresi"
+            value={email}
+            onChange={(e) => onChange(e)}
+            required
+          />
         </Form.Group>
         <Form.Group controlId="formBasicPassword">
-          <Form.Control type="password" placeholder="Şifreniz" />
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Şifre"
+            value={password}
+            onChange={(e) => onChange(e)}
+            required
+          />
         </Form.Group>
         <Form.Group controlId="formBasicPasswordCheck">
-          <Form.Control type="password" placeholder="Şifreyi bi teyit edelim" />
+          <Form.Control
+            type="password"
+            name="password2"
+            placeholder="Şifreyi bi teyit edelim"
+            value={password2}
+            onChange={(e) => onChange(e)}
+            required
+          />
         </Form.Group>
         <Form.Group controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Bizi anlıyor musunuz?" />
+          <Form.Check type="checkbox" label="Bizi anlıyor musun?" />
         </Form.Group>
         <Button variant="primary" type="submit">
           Kaybol
