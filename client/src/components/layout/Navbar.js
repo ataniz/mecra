524e82086ed3fb942fa26e2b23399566b7cc9af3
Ata Nizamoglu <nizamoglu@campus.tu-berlin.de>
2021-04-24 19:27:00 +0300

    logout redux

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,10 +1,34 @@
-import { React } from 'react';
+import { React, Fragment } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../../actions/auth';
+
 import { Button } from 'react-bootstrap';
 import { Navbar, Nav, Form, FormControl } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
-const NavBar = () => {
+const NavBar = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const authLinks = (
+    <Nav className="ml-auto">
+      <Nav.Link onClick={logout} href="#!" className="mr-2">
+        <FontAwesomeIcon icon={faSignOutAlt} className="mr-2" />
+        Çıkış Yap
+      </Nav.Link>
+    </Nav>
+  );
+
+  const guestLinks = (
+    <Nav className="ml-auto">
+      <Nav.Link href="/login" className="mr-2">
+        Giriş Yap
+      </Nav.Link>
+      <Nav.Link href="/register" className="mr-2">
+        Kaydol
+      </Nav.Link>
+    </Nav>
+  );
+
   return (
     <Navbar bg="dark" variant="dark" className="py-1">
       <Navbar.Brand href="/" style={{ color: '#ffafcc' }}>
@@ -21,16 +45,20 @@ const NavBar = () => {
           <FontAwesomeIcon icon={faSearch} />
         </Button>
       </Form>
-      <Nav className="ml-auto">
-        <Nav.Link href="/login" className="mr-2">
-          Giriş Yap
-        </Nav.Link>
-        <Nav.Link href="/register" className="mr-2">
-          Kaydol
-        </Nav.Link>
-      </Nav>
+      {!loading && (
+        <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+      )}
     </Navbar>
   );
 };
 
-export default NavBar;
+NavBar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, { logout })(NavBar);
