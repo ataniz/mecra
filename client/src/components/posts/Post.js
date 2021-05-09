4dbfffa1c0724002a3de2954f9e062acea3968af
Ata Nizamoglu <nizamoglu@campus.tu-berlin.de>
2021-05-09 19:18:26 +0300

    fixed console warnings

---
+++ client/src/components/posts/Post.js
@@ -0,0 +1,10 @@
+// import {
+//   convertFromRaw,
+//   EditorState,
+// } from 'draft-js';
+
+// const rawContent = /* get this value from db */;
+
+// const contentState = convertFromRaw(JSON.parse(rawContent));
+
+// const editorState = EditorState.createWithContent(blocks);
